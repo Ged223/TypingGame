568455e4dec920bb06aed5427bb13de67359d22d
Ged223 <gedsvk@gmail.com>
2021-11-03 14:37:28 +0100

    debug log print submitted message

--- Assets/MessageManager.cs
+++ Assets/MessageManager.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class MessageManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    // [SerializeField] - dont know what this does
+    public TMPro.TMP_InputField inputField;
+
+    private void Start()
     {
-        
+        inputField.onEndEdit.AddListener(val =>
+        {
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+                Debug.Log(inputField.GetComponentInChildren<TMPro.TMP_InputField>().text);
+        });
     }
 
     // Update is called once per frame

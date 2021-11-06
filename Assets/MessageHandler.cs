69fcc0452284ca121d7395ef4cd3cda41682eb0e
Ged223 <gedsvk@gmail.com>
2021-11-06 10:08:20 +0100

    Revert "renamed script messagehandler, will have one script per scene"
    This reverts commit 79728eaf0e550590dd0d9538c14324a79ecdd51d.

--- Assets/MessageHandler.cs
+++ Assets/MessageHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class MistakesLevelMessageHandler : MonoBehaviour
+public class MessageHandler : MonoBehaviour
 {
     // Start is called before the first frame update
     // [SerializeField] - dont know what this does

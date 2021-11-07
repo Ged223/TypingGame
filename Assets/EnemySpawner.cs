cc93a9e8388bd566f1a13252602d71fd74cc2fb2
Ged223 <gedsvk@gmail.com>
2021-11-07 13:04:05 +0100

    added enemy and enemy spawner

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private float nextSpawnTime;
+    [SerializeField]
+    private GameObject enemyPrefab;
+    [SerializeField]
+    private float spawnDelay = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (ShouldSpawn())
+            {
+                Spawn();
+            }
+        
+    }
+
+    private void Spawn()
+    {
+        nextSpawnTime = Time.time + spawnDelay;
+        Instantiate(enemyPrefab, transform.position, transform.rotation);
+    }
+
+    private bool ShouldSpawn()
+    {
+        return Time.time >= nextSpawnTime;
+    }
+}

f12277352b42a88b31993e53f835139fe617679c
Ged223 <gedsvk@gmail.com>
2022-02-10 17:17:00 +0100

    add color coding text to be more clear

--- Assets/TogglesManager.cs
+++ Assets/TogglesManager.cs
@@ -40,15 +40,24 @@ public class TogglesManager : MonoBehaviour
         if (SceneManager.GetActiveScene().name == "MainMenu")
         {
             GameObject.FindGameObjectWithTag("FirstMissionText").GetComponent<TMPro.TMP_Text>().SetText("Finish with 0 mistakes.\nCompleted: " + firstMissionDone.ToString());
+            GameObject.FindGameObjectWithTag("FirstMissionText").GetComponent<TMPro.TMP_Text>().color = firstMissionDone ? Color.green : Color.red;
             GameObject.FindGameObjectWithTag("SecondMissionText").GetComponent<TMPro.TMP_Text>().SetText("Finish with more frequent words.\nCompleted: " + secondMissionDone.ToString());
+            GameObject.FindGameObjectWithTag("SecondMissionText").GetComponent<TMPro.TMP_Text>().color = secondMissionDone ? Color.green : Color.red;
             GameObject.FindGameObjectWithTag("ThirdMissionText").GetComponent<TMPro.TMP_Text>().SetText("Finish with faster words.\nCompleted: " + thirdMissionDone.ToString());
+            GameObject.FindGameObjectWithTag("ThirdMissionText").GetComponent<TMPro.TMP_Text>().color = thirdMissionDone ? Color.green : Color.red;
             GameObject.FindGameObjectWithTag("FourthMissionText").GetComponent<TMPro.TMP_Text>().SetText("Finish with more words.\nCompleted: " + fourthMissionDone.ToString());
+            GameObject.FindGameObjectWithTag("FourthMissionText").GetComponent<TMPro.TMP_Text>().color = fourthMissionDone ? Color.green : Color.red;
             GameObject.FindGameObjectWithTag("FifthMissionText").GetComponent<TMPro.TMP_Text>().SetText("Complete all of the above missions in a single attempt.\nCompleted: " + fifthMissionDone.ToString());
+            GameObject.FindGameObjectWithTag("FifthMissionText").GetComponent<TMPro.TMP_Text>().color = fifthMissionDone ? Color.green : Color.red;
 
             GameObject.FindGameObjectWithTag("frwText").GetComponent<TMPro.TMP_Text>().SetText("Frequent words     /frw\n" + frequentWords.ToString());
+            GameObject.FindGameObjectWithTag("frwText").GetComponent<TMPro.TMP_Text>().color = frequentWords ? Color.green : Color.red;
             GameObject.FindGameObjectWithTag("fawText").GetComponent<TMPro.TMP_Text>().SetText("Fast words            /faw\n" + fasterWords.ToString());
-            GameObject.FindGameObjectWithTag("mowText").GetComponent<TMPro.TMP_Text>().SetText("More words           /mow\n" + moreWords.ToString());          
+            GameObject.FindGameObjectWithTag("fawText").GetComponent<TMPro.TMP_Text>().color = fasterWords ? Color.green : Color.red;
+            GameObject.FindGameObjectWithTag("mowText").GetComponent<TMPro.TMP_Text>().SetText("More words           /mow\n" + moreWords.ToString());
+            GameObject.FindGameObjectWithTag("mowText").GetComponent<TMPro.TMP_Text>().color = moreWords ? Color.green : Color.red;
             GameObject.FindGameObjectWithTag("sfxText").GetComponent<TMPro.TMP_Text>().SetText("Sound Effects	/sfx\n" + soundEffects.ToString());
+            GameObject.FindGameObjectWithTag("sfxText").GetComponent<TMPro.TMP_Text>().color = soundEffects ? Color.green : Color.red;
 
         }
     }
